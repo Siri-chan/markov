a6cf24be8e1d1bbc4da242347c20538fd0b0b342
Siri <xd42069xd69420@gmail.com>
2022-09-28 10:55:49 +0900

    Initial Commit
    Made markov chain generator

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Siri.markov {
+	class Program {
+		static void Main(string[] args) {
+			Config cfg = new ConfigBuilder().setWorkingDirectory("E:/markov_test").build();
+			//todo try/catch this and make sure file esxists
+			var checksum = GetChecksum(cfg.workingDir + "/list.txt");
+			Cache cache;
+			if (cfg.useCache){
+				cache = Cache.load(cfg);
+				if (checksum != cache.list_checksum)
+					cache = new Cache(checksum);
+			} else {
+				cache = new Cache(checksum);
+			}
+			string[] lines = System.IO.File.ReadAllLines(cfg.workingDir + "/list.txt");
+			if (/*cache.firstWords == new List<string>()*/ true) { //todo check why this doesnt work
+				foreach (string line in lines) {
+					//Regex for find dash-seperated word (with or without apostrophies). will add more acceptable characters if need be
+					//Console.WriteLine(line);
+					cache.firstWords.Add(Regex.Match(line, @"^([\w\-\\']+)").ToString());
+				}
+			}
+
+			var r = new Random();
+			int i = r.Next(cache.firstWords.Count);
+
+			string curWord = cache.firstWords[i];
+			string nextWord;
+
+			Console.WriteLine("First Word: " + curWord);
+
+			string buf = curWord;
+			while(true){
+				for (int k = 0; k < cfg.length; k++){
+					FreqTable freq;
+					if (cache.frequencies.ContainsKey(curWord) && cache.frequencies[curWord].curWord == curWord){
+						freq = cache.frequencies[curWord];
+						int j = r.Next(freq.words.Count);
+						nextWord = freq.words[j];
+						Console.WriteLine("Next Word: " + nextWord);
+					} else {
+						freq = makeTable(curWord, lines);
+						cache.frequencies.Add(curWord, freq);
+						int j = r.Next(freq.words.Count);
+						nextWord = freq.words[j];
+						Console.WriteLine("Next Word: " + nextWord);
+					}
+					buf += " " + nextWord;
+					curWord = nextWord;
+				}
+				Console.WriteLine(buf);
+				Console.WriteLine("Press Enter to Quit, or Space to Generate More");
+				var key = Console.ReadKey().Key;
+				while (key != ConsoleKey.Enter && key != ConsoleKey.Spacebar) { key = Console.ReadKey().Key; }
+				if(key == ConsoleKey.Enter) System.Environment.Exit(0);
+			}
+		}
+		static string GetChecksum(string filename) {
+			using (var md5 = System.Security.Cryptography.MD5.Create()) {
+				using (var stream = System.IO.File.OpenRead(filename)) {
+					var hash = md5.ComputeHash(stream);
+					return BitConverter.ToString(hash).Replace("-", "");
+				}
+			}
+		}
+		static FreqTable makeTable(string curWord, string[] lines) {
+			var table = new FreqTable();
+			table.curWord = curWord;
+			table.words = new List<string>();
+			List<string> _words = new List<string>();
+			foreach (string line in lines){
+				_words.AddRange(line.Split(" "));
+			}
+			for (int i = 0; i < _words.Count; i++){
+				if (_words[i] != curWord || _words[i+1] == null) continue;
+				table.words.Add(_words[i+1]);
+			}
+			return table;
+		}
+	}
+
+	public struct FreqTable {
+		public string curWord;
+		public List</*FreqPair*/ string> words; //pairs;
+	}
+
+	/*
+	public struct FreqPair {
+		public string nextWord;
+		public int freq;
+	}
+	*/
+
+	public class Cache {
+		public string list_checksum;
+		public List<string> firstWords = new List<string>();
+		public Dictionary<string, FreqTable> frequencies = new Dictionary<string, FreqTable>();
+		public static Cache load(Config cfg){
+			//use cfg to get working dir
+			return new Cache(""); // todo stub
+		}
+		public Cache(string checksum){
+			this.list_checksum = checksum;
+			// todo stub
+		}
+	}
+
+	public struct Config {
+		public bool useCache;
+		public string workingDir;
+		public int length;
+	}
+
+	public class ConfigBuilder {
+		bool useCache;
+		string workingDir;
+		int length;
+		public ConfigBuilder() {
+			useCache = true;
+			if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+				workingDir = Environment.GetEnvironmentVariable("LOCALAPPDATA");
+			} else {
+				workingDir = "~";
+			}
+			workingDir += "/.markov";
+			setWorkingDirectory(workingDir);
+			length = 10;
+		}
+
+		public ConfigBuilder setWorkingDirectory(string path, bool verbose = true) {
+			try {
+			    if (Directory.Exists(path)) {
+					if (verbose)
+						Console.WriteLine("Path exists already.");
+				} else {
+					Directory.CreateDirectory(path);
+				}
+				this.workingDir = path;
+			}
+			catch (Exception e) {
+				if (verbose)
+					Console.Error.WriteLine("Failed to Create Working Directory: {0}", e.ToString());
+			}
+
+			return this;
+		}
+
+		public ConfigBuilder setLength(int len) {
+			this.length = len;
+			return this;
+		}
+
+		public Config build() {
+			Environment.CurrentDirectory = workingDir;
+			var config = new Config();
+			config.useCache = this.useCache;
+			config.workingDir = this.workingDir;
+			config.length = this.length;
+			return config;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# markov
+A markov-chain generator.
+Place a plain text file into the directory specified in the config; then run using the dotnet sdk.
+Uses `.NET 6.0`.
+When I bother to make the implementation, it will cache prior results in the directory of the text list, meaning that for each run with the same list, the generations get faster and faster.
+
+## Examples
+These examples were generated using 10 words and the test_list.
+They are "emotional" quotes.
+
+```txt
+Take up and within us are dirty, the people you strong.
+```
+```txt
+Don't settle. As you up everything around us becomes better person.
+```
+
+## License
+Copyright (c) Kira "Siri" K, 2022.
+Licensed under the Mozilla Public License (MPL).
+Breach of this license is illegal under international Copyright law.
+See `LICENSE` for more details.
